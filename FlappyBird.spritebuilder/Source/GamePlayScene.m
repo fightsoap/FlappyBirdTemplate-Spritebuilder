c5ff32a375983c508f3ddf866f40b27679641a7b
fightsoap <dornerbrad@gmail.com>
2015-06-15 18:12:10 -0600

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,6 +9,8 @@
     // your code here
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
+    [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
@@ -31,7 +33,7 @@
 // put new methods here
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
     // this will get called every time the player touches the screen
-    [character flap];[self addObstacle]; timeSinceObstacle = 0.4f;
+    [character flap];
 }
 
 @end
